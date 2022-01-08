0940b80f9288c14323028a2562febfc4af57ded9
Aryza <88209926+Aryza23@users.noreply.github.com>
2022-01-09 01:42:18 +0700

    Update thumbnails.py

--- Yukki/Utilities/thumbnails.py
+++ Yukki/Utilities/thumbnails.py
@@ -1,5 +1,5 @@
 import os
-import random
+
 from os import path
 
 import aiofiles
@@ -24,32 +24,13 @@ async def gen_thumb(thumbnail, title, userid, theme, ctitle):
                 await f.write(await resp.read())
                 await f.close()
     image1 = Image.open(f"cache/thumb{userid}.jpg")
-    image2 = Image.open(f"Utils/{theme}.PNG")
+    image2 = Image.open(f"Utils/trpsnt.png")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
     image6 = image4.convert("RGBA")
     Image.alpha_composite(image5, image6).save(f"cache/temp{userid}.png")
     img = Image.open(f"cache/temp{userid}.png")
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("Utils/finalfont.ttf", 85)
-    font2 = ImageFont.truetype("Utils/finalfont.ttf", 60)
-    draw.text(
-        (20, 45),
-        f"Playing on: {ctitle[:14]}...",
-        fill="white",
-        stroke_width=1,
-        stroke_fill="white",
-        font=font2,
-    )
-    draw.text(
-        (25, 595),
-        f"{title[:27]}...",
-        fill="white",
-        stroke_width=2,
-        stroke_fill="white",
-        font=font,
-    )
     img.save(f"cache/final{userid}.png")
     os.remove(f"cache/temp{userid}.png")
     os.remove(f"cache/thumb{userid}.jpg")
