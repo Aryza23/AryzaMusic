acfcb0039ce05e07b124ea6e7a47194e7532c3dc
Aryza <88209926+Aryza23@users.noreply.github.com>
2022-01-09 00:12:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -20,9 +20,8 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/Aryza23/AryzaMusic"
-)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO")
+
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 if str(getenv("SUPPORT_CHANNEL")).strip() == "":
